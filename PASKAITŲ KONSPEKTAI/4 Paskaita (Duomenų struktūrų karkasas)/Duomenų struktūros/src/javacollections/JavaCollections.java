6ca1dccd4b19fb4b57f99df4617af6b5b86cd44a
Marius <m.gzegozevskis@gmail.com>
2016-10-15 18:43:05 +0300

    Paskaitų konspektai

---
+++ PASKAITŲ KONSPEKTAI/4 Paskaita (Duomenų struktūrų karkasas)/Duomenų struktūros/src/javacollections/JavaCollections.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javacollections;
+
+/**
+ *
+ * @author User
+ */
+public class JavaCollections {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        ALL_IN_1 all_in_1 = new ALL_IN_1();
+        all_in_1.iterator();
+        all_in_1.listIterator();
+        all_in_1.enumeration();
+        all_in_1.vector();
+        all_in_1.vectorIterator();
+        all_in_1.linkedList();
+        all_in_1.linkedListIterator();
+        all_in_1.hashTable();
+        all_in_1.hashTableValueSearch();
+        all_in_1.hashSet();
+        all_in_1.hashSetRead();
+        all_in_1.hashSetCopy();
+        all_in_1.treeSet();
+        all_in_1.treeSetIterator();
+        all_in_1.listTreeSet();
+        all_in_1.treeDuplicateEntry();
+        all_in_1.hashMap();
+        all_in_1.readHashMap();
+        all_in_1.collectionsAddAll();
+    }
+    
+}

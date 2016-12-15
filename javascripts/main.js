addab2979d2c8e8f1a66cb28aa337e0e0327e5ba
Marius Gžegoževskis <m.gzegozevskis@gmail.com>
2016-12-15 14:31:19 +0200

    Add files via upload

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
 console.log('This would be the main JS file.');
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+  }
+}
